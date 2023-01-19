c37b3acea61c21f5593dbbb27a8d1caebd13a59c
Pablo Aquino <pabloaquino1988@gmail.com>
2023-01-18 21:09:57 -0300

    Copyright icon added

--- src/layout/NavBar.jsx
+++ src/layout/NavBar.jsx
@@ -1,26 +1,26 @@
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import { Link } from "react-router-dom";
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
 
 export default function NavBar() {
   return (
-    <Navbar expand="lg">
-      <Navbar.Brand className="navbar-brand" href="/">
+    <Navbar expand='lg'>
+      <Navbar.Brand className='navbar-brand' href='/'>
         Santiago Aquino
       </Navbar.Brand>
       <Navbar.Toggle
-        aria-controls="basic-navbar-nav"
-        style={{ border: "none" }}
+        aria-controls='basic-navbar-nav'
+        style={{ border: 'none' }}
       />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="nav-link justify-content-end flex-grow-1">
-          <Nav.Link as={Link} to="/beauty">
+      <Navbar.Collapse id='basic-navbar-nav'>
+        <Nav className='nav-link justify-content-end flex-grow-1'>
+          <Nav.Link as={Link} to='/beauty'>
             Beauty
           </Nav.Link>
-          <Nav.Link as={Link} to="/retratos">
+          <Nav.Link as={Link} to='/retratos'>
             Retratos
           </Nav.Link>
-          <Nav.Link as={Link} to="/about">
+          <Nav.Link as={Link} to='/about'>
             About
           </Nav.Link>
         </Nav>
